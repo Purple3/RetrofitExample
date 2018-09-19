99162da48e5b90dc1a6a714908027e52380d8836
Pratyusha Vankayala <pratyushavankayala@gmail.com>
2018-09-18 20:11:15 -0700

    Initial commit

---
+++ app/src/main/java/com/purple3/retrofitexample/Api.java
@@ -0,0 +1,19 @@
+package com.purple3.retrofitexample;
+
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Belal on 10/2/2017.
+ */
+
+public interface Api {
+
+    String BASE_URL = "https://simplifiedcoding.net/demos/";
+
+    @GET("marvel")
+    Call<List<Hero>> getHeroes();
+}
